8ee4da17ac989d52cb9407992eb50b6283f8f68a
kuum97 <kuum97@icloud.com>
2023-04-08 20:14:40 +0900

    Start setting root page

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,51 @@
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { FaAirbnb, FaMoon } from "react-icons/fa";
 
 export default function Root() {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
-    <h1>
-      I'm Root
+    <Box>
+      <HStack
+        justifyContent={"space-between"}
+        py={"5"}
+        px={"10"}
+        borderBottomWidth={1}
+      >
+        <Box color={"red.500"}>
+          <FaAirbnb size={"48"} />
+        </Box>
+        <HStack spacing={"1rem"}>
+          <IconButton
+            variant={"ghost"}
+            aria-label="Toggle dark mode"
+            icon={<FaMoon />}
+          />
+          <Button onClick={onOpen}>Log in</Button>
+          <Button colorScheme={"red"}>Sign up</Button>
+        </HStack>
+        <Modal onClose={onClose} isOpen={isOpen}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Log in</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>hello </ModalBody>
+          </ModalContent>
+        </Modal>
+      </HStack>
       <Outlet />
-    </h1>
+    </Box>
   );
 }
